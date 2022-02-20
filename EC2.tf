b2750fcbf5e80fabe91b2c2df02a07c1477f32e2
Yousaf K Hamza <31238282+yousafkhamza@users.noreply.github.com>
2022-02-20 23:24:43 +0530

    userdata in builded

--- EC2.tf
+++ EC2.tf
@@ -10,7 +10,18 @@ resource "aws_instance" "ec2_instance" {
 #  key_name                    = var.key  #we are using password through userdata once you logged in please change on be half of you!
   subnet_id                   = tolist(data.aws_subnets.my_vpc.ids)[0]
   vpc_security_group_ids      = [ aws_security_group.sg_for_ec2.id ]
-  user_data 				           = file("userdata.sh")
+  user_data 				           = <<EOF
+#!/bin/bash
+
+echo "ClientAliveInterval 60" >> /etc/ssh/sshd_config
+echo "LANG=en_US.utf-8" >> /etc/environment
+echo "LC_ALL=en_US.utf-8" >> /etc/environment
+
+echo "${var.password_for_ec2}" | passwd root --stdin
+sed  -i 's/#PermitRootLogin yes/PermitRootLogin yes/' /etc/ssh/sshd_config
+sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+service sshd restart
+EOF
   tags                         = {
     Name                      = var.ec2_tag
   }
